654b7073dfd4cbb790ed09c6aaf90d1fd22ffe1d
Altaf Quadri <altafmquadri@gmail.com>
2020-02-22 19:07:36 -0500

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// pyramid = n => {
+//     const midpoint = Math.floor((2 * n - 1)/2)
+//     for (let r=0; r<n; r++) {
+//         let level = ''
+//         for (let c=0; c < 2*n-1; c++) {
+//             if (midpoint - r <=c && midpoint + r >= c) {
+//                 level += '#'
+//             } else {
+//                 level+= ' '
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
 
+
+pyramid = (n, r=0, level='')  => {
+    if (r === n) return
+    if (level.length === 2 * n -1) {
+        console.log(level)
+        return pyramid(n, r+1)
+    }
+    let midpoint= Math.floor((2 *n -1)/2)
+    if (midpoint - r <= level.length && midpoint + r >= level.length) {
+        level += '#'
+    } else {
+        level += ' '
+    }
+    pyramid(n, r, level)
+}
 module.exports = pyramid;
